dbc3b7901f28d5d07840b1d7c3dfaadcf72e9faa
Parpa-123 <paritoshsingh2004@outlook.com>
2026-01-11 06:35:54 +0530

    Modified built.sh to create superuser.

--- backend/build.sh
+++ backend/build.sh
@@ -2,10 +2,18 @@
 set -o errexit
 
 echo "Running migrations..."
-python manage.py migrate --noinput
+python manage.py migrate
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
+
+
+
+if [[ $CREATE_SUPERUSER ]];
+then
+    echo "Creating superuser..."
+    python manage.py createsuperuser --no-input
+fi
 
 echo "Starting Gunicorn..."
 exec gunicorn conf.asgi:application \
