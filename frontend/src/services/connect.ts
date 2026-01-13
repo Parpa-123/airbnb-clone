6e7cf9ddd7eff3d12d0cbc3b7affeadb340cb360
Parpa-123 <paritoshsingh2004@outlook.com>
2026-01-13 09:30:36 +0530

    Corrected the connect.ts to get the backend url during production.

--- frontend/src/services/connect.ts
+++ frontend/src/services/connect.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const isDev = import.meta.env.MODE === 'development';
-const myBaseUrl = isDev ?  import.meta.env.VITE_API_URL : "http://127.0.0.1:8000/api/";
+const myBaseUrl = isDev ? "http://127.0.0.1:8000/api/" : import.meta.env.VITE_API_URL;
 const axiosInstance = axios.create({
     baseURL: myBaseUrl,
     headers: {
